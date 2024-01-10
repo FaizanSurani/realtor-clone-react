86040bf1c676a9ef555de3188eff2c6a1d88017e
Faizan-Surani <suranifaizan52@gmail.com>
2024-01-10 18:54:37 +0530

    create the loader component

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { Navigate, Outlet } from "react-router-dom";
 import { useAuthStats } from "../hooks/useAuthStats";
+import Loader from "./Loader";
 
 export default function PrivateRoute() {
   const { loggedIn, checkingStatus } = useAuthStats();
   if (checkingStatus) {
-    return <h3>Loading....</h3>;
+    return <Loader />;
   }
   return loggedIn ? <Outlet /> : <Navigate to="/sign-in" />;
 }
