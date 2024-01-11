739b9f29be2c7579d6fe524c427f1ac252db4ff0
Faizan-Surani <suranifaizan52@gmail.com>
2024-01-11 23:55:04 +0530

    create a listing for sell or rent housing

--- src/pages/CreateListing.jsx
+++ src/pages/CreateListing.jsx
@@ -117,19 +117,19 @@ export default function CreateListing() {
         <div className="flex">
           <button
             type="button"
-            className={`mr-3 px-7 py-3 font-medium text-sm uppercase shadow-md rounded hover:shadow-lg focus:shadow-lg active:shadow-lg transition duration-150 ease-in-out w-full 
-            ${!parking ? "bg-white text-black" : "bg-slate-600 text-black"}`}
             id="parking"
             value={true}
+            className={`mr-3 px-7 py-3 font-medium text-sm uppercase shadow-md rounded hover:shadow-lg focus:shadow-lg active:shadow-lg transition duration-150 ease-in-out w-full 
+            ${!parking ? "bg-white text-black" : "bg-slate-600 text-black"}`}
             onClick={onChange}>
             Yes
           </button>
           <button
             type="button"
-            className={`m1-3 px-7 py-3 font-medium text-sm uppercase shadow-md rounded hover:shadow-lg focus:shadow-lg active:shadow-lg transition duration-150 ease-in-out w-full 
-            ${parking ? "bg-white text-black" : "bg-slate-600 text-black"}`}
             id="parking"
             value={false}
+            className={`m1-3 px-7 py-3 font-medium text-sm uppercase shadow-md rounded hover:shadow-lg focus:shadow-lg active:shadow-lg transition duration-150 ease-in-out w-full 
+            ${parking ? "bg-white text-black" : "bg-slate-600 text-black"}`}
             onClick={onChange}>
             No
           </button>
@@ -200,30 +200,47 @@ export default function CreateListing() {
             No
           </button>
         </div>
-        <div>
+        <div className="flex items-center mb-6">
           <div>
             <p className="text-lg font-semibold">Regular Price</p>
-            <input
-              type="regularPrice"
-              id="regularPrice"
-              value={regularPrice}
-              onChange={onChange}
-              required
-              className="w-full px-4 py-2 bg-white text-xl text-gray-700 border border-gray-300 focus:text-gray-700 focus:bg-white focus:border-slate-600 rounded transition duration-150 ease-in-out text-center"
-            />
+            <div className="flex justify-center items-center space-x-6">
+              <input
+                type="regularPrice"
+                id="regularPrice"
+                value={regularPrice}
+                onChange={onChange}
+                min="50"
+                max="400000000"
+                required
+                className="w-full px-4 py-2 bg-white text-xl text-gray-700 border border-gray-300 focus:text-gray-700 focus:bg-white focus:border-slate-600 rounded transition duration-150 ease-in-out text-center"
+              />
+              {type === "rent" && (
+                <div>
+                  <p className="text-md w-full whitespace-nowrap">$ / Month</p>
+                </div>
+              )}
+            </div>
           </div>
         </div>
-        <div>
-          <p className="text-lg font-semibold">Discounted Price</p>
-          <input
-            type="discountedPrice"
-            id="discountedPrice"
-            value={discountedPrice}
-            onChange={onChange}
-            required
-            className="w-full px-4 py-2 bg-white text-xl text-gray-700 border border-gray-300 focus:text-gray-700 focus:bg-white focus:border-slate-600 rounded transition duration-150 ease-in-out text-center"
-          />
-        </div>
+        {offer && (
+          <div className="flex items-center mb-6">
+            <div>
+              <p className="text-lg font-semibold">Discounted Price</p>
+              <div>
+                <input
+                  type="discountedPrice"
+                  id="discountedPrice"
+                  value={discountedPrice}
+                  onChange={onChange}
+                  min="50"
+                  max="400000000"
+                  required={offer}
+                  className="w-full px-4 py-2 bg-white text-xl text-gray-700 border border-gray-300 focus:text-gray-700 focus:bg-white focus:border-slate-600 rounded transition duration-150 ease-in-out text-center"
+                />
+              </div>
+            </div>
+          </div>
+        )}
       </form>
     </main>
   );
