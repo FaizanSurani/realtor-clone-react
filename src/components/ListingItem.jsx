4e89e8cf7dbf8585a35c55b80d0c52ed0df1b208
Faizan-Surani <suranifaizan52@gmail.com>
2024-01-16 14:29:50 +0530

    Complete the listingitem component

--- src/components/ListingItem.jsx
+++ src/components/ListingItem.jsx
@@ -1,5 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import Moment from "react-moment";
 
 export default function ListingItem({ id, listing }) {
-  return <div>{listing.name}</div>;
+  return (
+    <li>
+      <Link to={`/category/${listing.type}/${id}`}>
+        <img src={listing.imgUrls[0]} alt="" />
+        <Moment>{listing.timestamp?.toDate()}</Moment>
+      </Link>
+    </li>
+  );
 }
