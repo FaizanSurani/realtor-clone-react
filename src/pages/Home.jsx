3a31f84dea69d276ee059b55f91a20ae81888e5a
Faizan-Surani <suranifaizan52@gmail.com>
2024-01-22 22:13:55 +0530

    Complete home page by adding recent offers places for rent and place for sale

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Slider from "../components/Slider";
+import {
+  collection,
+  getDocs,
+  limit,
+  orderBy,
+  query,
+  where,
+} from "firebase/firestore";
+import { db } from "../Firebase";
 
 export default function Home() {
+  const [offerListings, setOfferListings] = useState(null);
+
+  useEffect(() => {
+    const fetchListings = async () => {
+      try {
+        const listingsRef = collection(db, "listings");
+        const q = query(
+          listingsRef,
+          where("offer", "==", true),
+          orderBy("timestamp", "desc"),
+          limit(4)
+        );
+        const docSnap = await getDocs(q);
+        if (docSnap.exists()) {
+          setOfferListings(docSnap.data());
+        }
+      } catch (error) {}
+    };
+    fetchListings();
+  }, []);
+
   return (
     <div>
       <Slider />
